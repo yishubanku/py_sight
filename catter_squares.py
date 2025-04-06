860e25c39696a87b6a33a85aef2d8da8d1284f6f
zhuweijie <riiwfnhsou@outlook.com>
2025-04-06 15:27:59 +0800

    feat:使用scatter方法绘制散点图

---
+++ catter_squares.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+
+
+# 指定中文字体，否则会出现显示问题
+plt.rcParams['font.sans-serif'] = ['SimHei']
+
+x_values = [1, 2, 3, 4, 5]
+y_values = [1, 4, 9, 16, 25]
+
+plt.style.use('ggplot')
+fig, ax = plt.subplots()
+ax.scatter(x_values, y_values, s=200)
+
+# 设置图表标题并给坐标轴加上标签
+ax.set_title("平方数", fontsize=24)
+ax.set_xlabel("值", fontsize=14)
+ax.set_ylabel("值的平方", fontsize=14)
+
+# 设置刻度标记的大小
+ax.tick_params(axis='both', which='major', labelsize=14)
+
+plt.show()

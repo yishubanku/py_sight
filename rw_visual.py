79bd65eade804d39cccb0b458341273040fac3c5
zhuweijie <riiwfnhsou@outlook.com>
2025-04-06 16:19:33 +0800

    feat:突出绘制起点和终点

--- rw_visual.py
+++ rw_visual.py
@@ -15,6 +15,11 @@ while True:
     ax.scatter(rw.x_values, rw.y_values, c=point_numbers,
                cmap=plt.cm.Blues, edgecolors='none', s=15)
 
+    # 突出起点和终点
+    ax.scatter(0, 0, c='green', edgecolors='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red',
+               edgecolors='none', s=100)
+
     plt.show()
 
     keep_running = input("Make another walk? (y/n): ")

5311c6a07caffaffac9363c9fe300145d030cf63
zhuweijie <riiwfnhsou@outlook.com>
2025-04-07 18:07:36 +0800

    feat:同时掷2个骰子

--- die_visual.py
+++ die_visual.py
@@ -5,27 +5,29 @@ from die import Die
 
 
 # 创建一个D6骰子
-die = Die()
+die_1 = Die()
+die_2 = Die()
 
 # 掷几次骰子并把结果存在一个列表里
 results = []
 for roll_num in range(1000):
-    result = die.roll()
+    result = die_1.roll() + die_2.roll()
     results.append(result)
 
 # 分析结果
 frequencies = []
-for value in range(1, die.num_sides+1):
+max_result = die_1.num_sides + die_2.num_sides
+for value in range(1, max_result+1):
     frequency = results.count(value)
     frequencies.append(frequency)
 
 # 可视化结果
-x_values = list(range(1, die.num_sides+1))
+x_values = list(range(1, max_result+1))
 data = [Bar(x=x_values, y=frequencies)]
 
 x_axis_config = {'title': '结果'}
 y_axis_config = {'title': '结果的频率'}
-my_layout = Layout(title='掷一个D6 1000次的结果',
+my_layout = Layout(title='掷两个D6 1000次的结果',
                    xaxis=x_axis_config, yaxis=y_axis_config)
 offline.plot({'data': data, 'layout': my_layout},
-             filename='d6.html')
+             filename='d6_d6.html')

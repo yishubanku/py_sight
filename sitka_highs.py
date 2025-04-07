e53920c537a661ee76d6037c82400b9a9fc324e4
zhuweijie <riiwfnhsou@outlook.com>
2025-04-07 18:54:55 +0800

    feat:使用更大时间范围的数据

--- sitka_highs.py
+++ sitka_highs.py
@@ -2,7 +2,7 @@ import csv
 import matplotlib.pyplot as plt
 from datetime import datetime
 
-filename = 'data/sitka_weather_07-2018_simple.csv'
+filename = 'data/sitka_weather_2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
@@ -28,7 +28,7 @@ fig, ax = plt.subplots()
 ax.plot(dates, highs, c='red')
 
 # 设置图形的格式
-ax.set_title("2018年7月每日最高温度", fontsize=24)
+ax.set_title("2018年每日最高温度", fontsize=24)
 ax.set_xlabel('', fontsize=16)
 ax.set_ylabel('温度 (F)', fontsize=16)
 ax.tick_params(axis='both', which='major', labelsize=16)

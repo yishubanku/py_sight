e19a81f808843b283dd7925109e8428f8012d9b3
zhuweijie <riiwfnhsou@outlook.com>
2025-04-07 18:13:08 +0800

    feat:同时掷2个面数不同的骰子

--- die_visual.py
+++ die_visual.py
@@ -6,11 +6,11 @@ from die import Die
 
 # 创建一个D6骰子
 die_1 = Die()
-die_2 = Die()
+die_2 = Die(10)
 
 # 掷几次骰子并把结果存在一个列表里
 results = []
-for roll_num in range(1000):
+for roll_num in range(50_000):
     result = die_1.roll() + die_2.roll()
     results.append(result)
 
@@ -25,9 +25,9 @@ for value in range(1, max_result+1):
 x_values = list(range(1, max_result+1))
 data = [Bar(x=x_values, y=frequencies)]
 
-x_axis_config = {'title': '结果'}
+x_axis_config = {'title': '结果', 'dtick': 1}
 y_axis_config = {'title': '结果的频率'}
-my_layout = Layout(title='掷两个D6 1000次的结果',
+my_layout = Layout(title='掷一个D6和一个D10 50000次的结果',
                    xaxis=x_axis_config, yaxis=y_axis_config)
 offline.plot({'data': data, 'layout': my_layout},
-             filename='d6_d6.html')
+             filename='d6_d10.html')

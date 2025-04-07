4c571940f8a8bf5affcf3086751a52d14be9e6b3
zhuweijie <riiwfnhsou@outlook.com>
2025-04-07 18:27:37 +0800

    feat:提取并读取数据

--- sitka_highs.py
+++ sitka_highs.py
@@ -5,4 +5,15 @@ filename = 'data/sitka_weather_07-2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
-    print(header_row)
+
+    # 打印文件头及其位置
+    # for index, column_header in enumerate(header_row):
+    #     print(index, column_header)
+
+    # 从文件中获取最高温度
+    highs = []
+    for row in reader:
+        high = int(row[5])
+        highs.append(high)
+
+print(highs)
